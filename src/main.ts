036740625ab6ae77735403541f65e991754fb490
Itay Maman <imaman@users.noreply.github.com>
2019-09-25 16:14:14 +0300

    push

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import * as sourceMapSupport from 'source-map-support';
 sourceMapSupport.install();
 
+/* tslint:disable:no-submodule-imports */
+import * as git from 'simple-git/promise'
 import * as yargs from 'yargs';
 
-function createPr(args) {    
+function createPr(args) { 
     console.log(JSON.stringify(args))
+}
+
+async function push(args) {
+    const bs = await git().branch(["-vv"])
+    console.log('bs=' + JSON.stringify(bs, null, 2))
 }
 
 /* tslint:disable:no-shadowed-variable no-unused-expression */
@@ -12,6 +19,9 @@ yargs
     .usage('<cmd> [options]')
     .version('1.0.0')
     .strict()
+    .command('push', 'push your branch', yargs => {
+        // blah blah blah
+    }, push)
     .command('pr', 'Creates a PR', yargs => {
         // specFileAndSectionOptions(yargs);
         // yargs.option('teleporting', {
