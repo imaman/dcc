ab10bdfe3de5598b4ca52559a1786e2b68aab6cc
Itay Maman <imaman@users.noreply.github.com>
2019-09-26 16:59:54 +0300

    create and list prs

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,26 @@ import * as yargs from 'yargs';
 const octokit = (() => {
     let ret: Octokit|undefined
     return () => {
-        const auth = fs.readFileSync(path.resolve(__dirname, '../.conf'), 'utf-8')
+        const auth = fs.readFileSync(path.resolve(__dirname, '../.conf'), 'utf-8').split('\n')[0].trim()
         ret = ret || new Octokit({ auth });        
         return ret
     }
 })()
 
+async function getUser() {
+    const kit = octokit()
+    const d = await kit.users.getAuthenticated()
+    return d.data.login
+}
+
+// getUser().then(x => console.log(x, null, 2)).catch(e => console.log('BAD', e))
+
 async function createPr(args) { 
     if (!args.title) {
         throw new Error('Title must be specified')
     }
+
+    noUncommittedChanges()
 
     const b = await getBranch()
     const r = await getRepo()
@@ -34,27 +44,48 @@ async function createPr(args) {
         repo: r.name,
         title: args.title
     }
-    await kit.pulls.create(req)
-    console.log(JSON.stringify(args))
+    const resp = await kit.pulls.create(req)
+    console.log(JSON.stringify(resp))
 }
 
-async function getPrs(args) { 
-    const r = await getRepo()
+async function listPrs() { 
+    const arr = await Promise.all([getRepo(), getUser()])
+    const r = arr[0]
+    const user = arr[1]
     const kit = octokit()
 
     const req: Octokit.PullsListParams = {
-        // base: 'master',
-        // head: b.name,
         owner: r.owner,
         repo: r.name,
     }
-    await kit.pulls.list(req)
-    console.log(JSON.stringify(args))
+    const resp = await kit.pulls.list(req)
+    const prs = resp.data.map(curr => ({
+            user: curr.user.login, 
+            title: curr.title, 
+            url: `https://github.com/${r.owner}/${r.name}/pull/${curr.number}`,
+            body: curr.body,
+            updatedAt: curr.updated_at,
+            createdAt: curr.created_at,
+            // mergedAt: curr.merged_at,
+            // closedAt: curr.closed_at,
+            number: curr.number,
+            state: curr.state
+        }))
+
+    prs.sort((a, b) => b.updatedAt.localeCompare(a.updatedAt))
+
+    const ret = prs.filter(curr => curr.user === user)
+    console.log(JSON.stringify(ret, null, 2))
 }
 
 async function getBranch() {
     const bs = await git().branch(["-vv"])
     return bs.branches[bs.current]
+}
+
+async function noUncommittedChanges() {
+    const d = await git().diffSummary()
+    console.log(JSON.stringify(d, null, 2))
 }
 
 async function getRepo() {
@@ -92,9 +123,8 @@ async function getRepo() {
     return repos[0]
 }
 
-getPrs({}).then(x => console.log(x)).catch(e => console.error('NO-NO-NO', e))
-
 async function push(args) {
+    noUncommittedChanges()
     const b = await getBranch()
 
     const inst = git()
@@ -111,6 +141,7 @@ yargs
     .command('push', 'push your branch', yargs => {
         // blah blah blah
     }, push)
+    .command('list', 'List PRs', yargs => {}, listPrs)
     .command('pr [options]', 'Creates a PR', yargs => {
         // specFileAndSectionOptions(yargs);
         yargs.option('title', {
