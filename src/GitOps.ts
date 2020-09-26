670b3d6d1db37b4ec163b0563c415aaa35af4907
Itay Maman <94941+imaman@users.noreply.github.com>
2020-09-26 15:54:00 +0300

    fix types (#8)
    * add missing dep
    
    * add missing dep
    
    * install a couple of @types modules
    
    * touch
    
    * delete spawner
    
    * touch
    
    * _
    
    * _
    
    * add types
    
    * revive test
    
    Co-authored-by: Itay Maman <imaman@users.noreply.github.com>

--- src/GitOps.ts
+++ src/GitOps.ts
@@ -1,77 +1,117 @@
 import { SimpleGit } from 'simple-git/promise'
 import * as child_process from 'child_process'
 
+interface BranchInfo {
+  current: string
+  name: string
+  commit: string
+  label: string
+}
+
+interface RepoInfo {
+  owner: string
+  name: string
+}
+
+interface LogInfo {
+  hash: string
+  date: string
+  message: string
+  refs: string
+  body: string
+  author_name: string
+  author_email: string
+}
+
+interface CommitInfo {
+  ordinal: number
+  data: LogInfo
+}
+
 function stopMe(message: string) {
-    console.log(message)
-    process.exit(-1)
+  // eslint-disable-next-line no-console
+  console.log(message)
+  // eslint-disable-next-line no-process-exit
+  process.exit(-1)
 }
 
 export class GitOps {
+  constructor(private readonly git: SimpleGit) {}
 
-    constructor(private readonly git: SimpleGit) {}
-
-    async describeCommit(sha: string) {
-        const log = await this.git.log()
-        const index = log.all.findIndex(curr => curr.hash === sha)
-        if (index < 0) {
-            return undefined
-        }
-
-        return {ordinal: index, data: log.all[index]}
+  async describeCommit(sha: string): Promise<CommitInfo | undefined> {
+    const log = await this.git.log()
+    const index = log.all.findIndex(curr => curr.hash === sha)
+    if (index < 0) {
+      return undefined
     }
 
-    async getBranch() {
-        const bs = await this.git.branch(["-vv"])
-        return bs.branches[bs.current]
-    }
-    
-    async noUncommittedChanges() {
-        const d = await this.git.diffSummary()
-        if (d.files.length) {
-            stopMe('you have uncommitted changes')
-        }
-    }
-    
-    async push() {
-        await this.noUncommittedChanges()    
-        const b = await this.getBranch()
-    
-        const temp = child_process.spawnSync('git', ['push', '--set-upstream', 'origin', b.name], {stdio: 'inherit'})
-        if (temp.status !== 0) {
-          throw new Error(`exit code (git push) is ${temp.status}`)
-        }
-    } 
+    const data = log.all[index]
+    return { ordinal: index, data }
+  }
 
-    async getRepo() {
-        const r = await this.git.remote(['-v'])
-        // "origin\tgit@github.com:imaman/dcc.git (fetch)\norigin\tgit@github.com:imaman/dcc.git (push)\n"
-        if (typeof r !== 'string') {
-            throw new Error('Expected a string')
-        }
-        const repos = r.split('\n')
-            .map(line => line && line.split('\t')[1].split(':')[1].split(' ')[0].split('/'))
-            .filter(Boolean)
-            .map(s => ({owner: s[0], name: s[1]}))
-    
-        for (const curr of repos) {
-            if (curr.name.endsWith('.git')) {
-              curr.name = curr.name.substr(0, curr.name.length - 4)
-            }
-        }
-    
-        if (!repos.length) {
-            throw new Error('No repo found')
-        }
-    
-        for (let i = 1; i < repos.length; ++i) {
-            const curr = repos[i];
-            const prev = repos[i - 1]
-            if (curr.name !== prev.name || curr.owner !== prev.owner) {
-                throw new Error(`More than one repo ("${JSON.stringify(prev)}", "${JSON.stringify(curr)}") was reported ` 
-                          + 'by <git remote -v>')
-            }
-        }
-    
-        return repos[0]
-    }        
+  async getBranch(): Promise<BranchInfo> {
+    const bs = await this.git.branch(['-vv'])
+    return bs.branches[bs.current]
+  }
+
+  async noUncommittedChanges(): Promise<void> {
+    const d = await this.git.diffSummary()
+    if (d.files.length) {
+      stopMe('you have uncommitted changes')
+    }
+  }
+
+  async push(): Promise<void> {
+    await this.noUncommittedChanges()
+    const b = await this.getBranch()
+
+    const temp = child_process.spawnSync('git', ['push', '--set-upstream', 'origin', b.name], { stdio: 'inherit' })
+    if (temp.status !== 0) {
+      throw new Error(`exit code (git push) is ${temp.status}`)
+    }
+  }
+
+  async getRepo(): Promise<RepoInfo> {
+    const r = await this.git.remote(['-v'])
+    // "origin\tgit@github.com:imaman/dcc.git (fetch)\norigin\tgit@github.com:imaman/dcc.git (push)\n"
+    if (typeof r !== 'string') {
+      throw new Error('Expected a string')
+    }
+    const repos = r
+      .split('\n')
+      .map(
+        line =>
+          line &&
+          line
+            .split('\t')[1]
+            .split(':')[1]
+            .split(' ')[0]
+            .split('/'),
+      )
+      .filter(Boolean)
+      .map(s => ({ owner: s[0], name: s[1] }))
+
+    for (const curr of repos) {
+      if (curr.name.endsWith('.git')) {
+        curr.name = curr.name.substr(0, curr.name.length - 4)
+      }
+    }
+
+    if (!repos.length) {
+      throw new Error('No repo found')
+    }
+
+    for (let i = 1; i < repos.length; ++i) {
+      const curr = repos[i]
+      const prev = repos[i - 1]
+      if (curr.name !== prev.name || curr.owner !== prev.owner) {
+        throw new Error(
+          `More than one repo ("${JSON.stringify(prev)}", "${JSON.stringify(curr)}") was reported ` +
+            'by <git remote -v>',
+        )
+      }
+    }
+
+    return repos[0]
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,116 +2,136 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as Octokit from '@octokit/rest'
 
-import * as sourceMapSupport from 'source-map-support';
-sourceMapSupport.install();
+import * as sourceMapSupport from 'source-map-support'
+sourceMapSupport.install()
 
 import * as git from 'simple-git/promise'
-import * as yargs from 'yargs';
+import * as yargs from 'yargs'
+import { Arguments } from 'yargs'
 
 import { GithubOps } from './GithubOps'
 import { GitOps } from './GitOps'
 
-const auth = fs.readFileSync(path.resolve(__dirname, '../.conf'), 'utf-8').split('\n')[0].trim()
-const octoKit = new Octokit({ auth });        
+const auth = fs
+  .readFileSync(path.resolve(__dirname, '../.conf'), 'utf-8')
+  .split('\n')[0]
+  .trim()
+const octoKit = new Octokit({ auth })
 
 const gitOps = new GitOps(git())
 const githubOps = new GithubOps(octoKit, gitOps)
 
 function format(s: string, n: number) {
-    if (s.length > n) {
-        return s.substr(0, n)
-    }
+  if (s.length > n) {
+    return s.substr(0, n)
+  }
 
-    return s.padEnd(n)
+  return s.padEnd(n)
 }
 
-function launch(f: (a: any) => Promise<void>) {
-    return (args: any) => {
-        if (args.dir) {
-            process.chdir(args.dir)
-        }
-        return f(args)
+function launch(f: (a: Arguments) => Promise<void>) {
+  return (args: Arguments) => {
+    if (args.dir) {
+      process.chdir(args.dir)
     }
+    return f(args)
+  }
 }
 
-async function listPrs() { 
-    const d = await githubOps.listPrs()
-    for (const curr of d) {
-        console.log(`${curr.updatedAt} ${('#' + curr.number).padStart(6)} ${format(curr.user, 10)} ${format(curr.title, 60)} ${curr.url}`)
-    }
+/* eslint-disable no-console */
+async function listPrs() {
+  const d = await githubOps.listPrs()
+  for (const curr of d) {
+    console.log(
+      `${curr.updatedAt} ${('#' + curr.number).padStart(6)} ${format(curr.user, 10)} ${format(curr.title, 60)} ${
+        curr.url
+      }`,
+    )
+  }
 }
 
-async function listMerged(args) { 
-    const d = await githubOps.listMerged(args.user)
+async function listMerged(args: Arguments) {
+  const d = await githubOps.listMerged(args.user)
 
-    for (const curr of d) {
-        console.log(`${curr.mergedAt} ${('#' + curr.number).padStart(6)} ${format(curr.user, 10)} ${format(curr.title, 60)} ${curr.url}`)
-    }
+  for (const curr of d) {
+    console.log(
+      `${curr.mergedAt} ${('#' + curr.number).padStart(6)} ${format(curr.user, 10)} ${format(curr.title, 60)} ${
+        curr.url
+      }`,
+    )
+  }
 }
 
 async function push() {
-    await gitOps.push()
+  await gitOps.push()
 }
 
-async function createPr(args) {
-    await githubOps.createPr(args.title)
+async function createPr(args: Arguments) {
+  await githubOps.createPr(args.title)
 }
 
 async function info() {
-    const o = await githubOps.listChecks()
+  const o = await githubOps.listChecks()
 
-    console.log(process.cwd())
-    console.log('Pushed: HEAD' + (Boolean(o.commit.ordinal) ? `~${o.commit.ordinal}` : ''))
-    console.log('Commit: ' + o.commit.data.hash)
-    console.log('Message: ' + o.commit.data.message.substr(0, 60))
+  console.log(process.cwd())
+  console.log('Pushed: HEAD' + (o.commit.ordinal ? `~${o.commit.ordinal}` : ''))
+  console.log('Commit: ' + o.commit.data.hash)
+  console.log('Message: ' + o.commit.data.message.substr(0, 60))
 
-    console.log()
-    const notPassingRequired = o.statuses.filter(curr => curr.required).filter(curr => curr.state !== 'success')
-    console.log(`Required checks pass? ${notPassingRequired.length ? 'no' : 'YES'}`)
+  console.log()
+  const notPassingRequired = o.statuses.filter(curr => curr.required).filter(curr => curr.state !== 'success')
+  console.log(`Required checks pass? ${notPassingRequired.length ? 'no' : 'YES'}`)
 
-    if (notPassingRequired.length) {
-        for (const curr of notPassingRequired) {
-            console.log('  ' + curr.context + '? ' + curr.state)
-        }
+  if (notPassingRequired.length) {
+    for (const curr of notPassingRequired) {
+      console.log('  ' + curr.context + '? ' + curr.state)
     }
+  }
 
-    console.log()
-    const notPassingOptional = o.statuses.filter(curr => !curr.required).filter(curr => curr.state !== 'success')
-    console.log(`Optional checks pass? ${notPassingOptional.length ? 'no' : 'YES'}`)
-    if (notPassingOptional.length) {
-        for (const curr of notPassingOptional) {
-            console.log('  ' + curr.context + '? ' + curr.state)
-        }
+  console.log()
+  const notPassingOptional = o.statuses.filter(curr => !curr.required).filter(curr => curr.state !== 'success')
+  console.log(`Optional checks pass? ${notPassingOptional.length ? 'no' : 'YES'}`)
+  if (notPassingOptional.length) {
+    for (const curr of notPassingOptional) {
+      console.log('  ' + curr.context + '? ' + curr.state)
     }
+  }
 }
 
 /* tslint:disable:no-shadowed-variable no-unused-expression */
 yargs
-    .usage('<cmd> [options]')
-    .version('1.0.0')
-    .strict()
-    .option('dir', {
-        alias: 'd',
-        describe: 'directroy to run at',
-        type: 'string'
-    })
-    .command('info', 'CI details', yargs => {}, launch(info))
-    .command('push', 'push your branch', yargs => {}, launch(push))
-    .command('prs', 'List currently open PRs', yargs => {}, launch(listPrs))
-    .command('merged', 'List recently merged PRs', yargs => {
-        yargs.option('user', {
-            alias: 'u',
-            describe: 'Shows only PR from that GitHub user ID. If omiited shows from all users.',
-            type: 'string'
-        });
-    }, launch(listMerged))
-    .command('pr [options]', 'Creates a PR', yargs => {
-        yargs.option('title', {
-            alias: 't',
-            describe: 'A one line summary of this PR',
-            type: 'string'
-        });
-    }, launch(createPr))
-    .help()
-    .showHelpOnFail(false, "Specify --help for available options")
-    .argv;
+  .usage('<cmd> [options]')
+  .version('1.0.0')
+  .strict()
+  .option('dir', {
+    alias: 'd',
+    describe: 'directroy to run at',
+    type: 'string',
+  })
+  .command('info', 'CI details', a => a, launch(info))
+  .command('push', 'push your branch', a => a, launch(push))
+  .command('prs', 'List currently open PRs', a => a, launch(listPrs))
+  .command(
+    'merged',
+    'List recently merged PRs',
+    yargs =>
+      yargs.option('user', {
+        alias: 'u',
+        describe: 'Shows only PR from that GitHub user ID. If omiited shows from all users.',
+        type: 'string',
+      }),
+    launch(listMerged),
+  )
+  .command(
+    'pr [options]',
+    'Creates a PR',
+    yargs =>
+      yargs.option('title', {
+        alias: 't',
+        describe: 'A one line summary of this PR',
+        type: 'string',
+      }),
+    launch(createPr),
+  )
+  .help()
+  .showHelpOnFail(false, 'Specify --help for available options').argv
